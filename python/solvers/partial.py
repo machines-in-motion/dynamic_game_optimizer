818f006d9d253c588841120441c243051f96e921
Bilal Hammoud <bah436@nyu.edu>
2022-02-25 12:03:28 -0500

    all computations work, merit function implementation remains

--- python/solvers/partial.py
+++ python/solvers/partial.py
@@ -72,17 +72,15 @@ class PartialDGSolver(SolverAbstract):
         for t, d in enumerate(self.measurement_trajectory.runningDatas):
             self.R[t][:,:] = d.R 
 
-
     def computeDirection(self, recalc=True):
         if recalc:
             self.calc()
 
         self._estimation_forward()
-        # self._control_backward()
-        # self._coupling()
-        # self._estimation_backward()
-        # self._control_forward()
-
+        self._control_backward()
+        self._coupling()
+        self._estimation_backward()
+        self._control_forward()
 
     def _estimation_forward(self):
         data0 = self.measurement_trajectory.runningDatas[0]  
@@ -111,30 +109,73 @@ class PartialDGSolver(SolverAbstract):
             self.xhat[t+1][:] = self.K_filter[t+1].dot(self.gammas[t+1]) \
                         +(np.eye(pmodel.state.ndx) - self.K_filter[t+1].dot(mdata.Hx)).dot(pdata.Fx.dot(self.xhat[t]) - self.ws[t+1]) \
                         + self.P[t+1].dot(self.invQ[t+1]).dot(pdata.Fx).dot(np.linalg.inv(E)).dot(self.mu*pdata.Lxx.dot(self.xhat[t]) + self.mu*pdata.Lx)
-                    
-            # 
-
-
-    def _control_forward(self):
-        raise NotImplementedError("solve method not implemented yet") 
-
-    def _estimation_backward(self):
-        raise NotImplementedError("solve method not implemented yet") 
-
+ 
     def _control_backward(self):
-        raise NotImplementedError("solve method not implemented yet") 
+        self.Vxx[-1][:,:] = self.problem.terminalData.Lxx
+        self.vx[-1][:] = self.problem.terminalData.Lx 
+        for t, (model, data) in rev_enumerate(zip(self.problem.runningModels[self.split_t:],
+                                                  self.problem.runningDatas[self.split_t:])):
+            aux0 = np.eye(model.state.ndx) - self.mu*self.Vxx[t+1].dot(self.Q[t+1]) 
+            Lb = scl.cho_factor(aux0, lower=True) 
+            aux1 = scl.cho_solve(Lb, self.Vxx[t+1])
+            aux2 = scl.cho_solve(Lb, self.vx[t+1])
+            Quu = data.Luu + data.Fu.T.dot(aux1).dot(data.Fu) 
+            Qux = data.Lxu.T + data.Fu.T.dot(aux1).dot(data.Fx)
+            Qu = data.Lu + data.Fu.T.dot(aux2) - data.Fu.T.dot(aux1).dot(self.ws[t+1])
+            #
+            Lb_uu = scl.cho_factor(Quu, lower=True)  
+            self.K[t][:,:] = scl.cho_solve(Lb_uu, Qux)
+            self.k[t][:] = scl.cho_solve(Lb_uu, Qu)
+            self.Vxx[t][:,:] = data.Lxx + data.Fx.T.dot(aux1).dot(data.Fx) - Qux.T.dot(self.K[t])
+            self.vx[t][:] =  data.Lx + data.Fx.T.dot(aux2 - aux1.dot(self.ws[t+1])) - Qux.T.dot(self.k[t])
 
     def _coupling(self):
-        raise NotImplementedError("this shit is not implemented yet")
+        t = self.split_t 
+        aux = np.eye(self.problem.runningModels[t].state.ndx) - self.mu*self.P[t].dot(self.Vxx[t])
+        Lb = scl.cho_factor(aux, lower=True) 
+        self.dx[t] = scl.cho_solve(Lb, self.xhat[t] + self.mu*self.P[t].dot(self.vx[t]))
 
-    def computeUpdates(self): 
-        raise NotImplementedError("solve method not implemented yet")
+    def _estimation_backward(self):
+        for t, (model, data) in rev_enumerate(zip(self.problem.runningModels[:self.split_t],
+                                                  self.problem.runningDatas[:self.split_t])):
+            Pinv = np.linalg.inv(self.P[t])
+            right = Pinv.dot(self.xhat[t]) + data.Fx.T.dot(self.invQ[t+1]).dot(self.ws[t+1]+ self.dx[t+1]) + self.mu*data.Lx
+            Lb = scl.cho_factor(Pinv + data.Fx.T.dot(self.invQ[t+1]).dot(data.Fx) - self.mu*data.Lxx, lower=True)  
+            self.dx[t] = scl.cho_solve(Lb, right) 
 
+    def _control_forward(self):
+        for t, (model, data) in enumerate(zip(self.problem.runningModels[self.split_t:],
+                                                  self.problem.runningDatas[self.split_t:])):
+            self.du[t] = -self.K[t].dot(self.dx[t]) - self.k[t]
+            right = self.mu*self.Q[t+1].dot(self.vx[t]) + data.Fx.dot(self.dx[t]) + data.Fu.dot(self.du[t]) -self.ws[t]
+            Lb = scl.cho_factor(np.eye(model.state.ndx) - self.mu*self.Q[t+1].dot(self.Vxx[t+1])) 
+            self.dx[t+1] = scl.cho_solve(Lb, right)
+    
     def tryStep(self, alpha):
-        return 0. 
+        self.merit_try = 0. 
+        for t, (model, data) in enumerate(zip(self.problem.runningModels,self.merit_runningDatas)):
+            self.xs_try[t][:] = model.state.integrate(self.xs[t], alpha*self.dx[t])
+            self.us_try[t][:] = self.us[t] + alpha*self.du[t]
+            model.calc(data, self.xs_try[t], self.us_try[t])
+            model.calcDiff(data, self.xs_try[t], self.us_try[t]) 
+        
+        self.xs_try[-1][:] = self.problem.terminalModel.state.integrate(self.xs[-1], alpha*self.dx[-1])
+        self.ws_try[-1][:]  = self.problem.terminalModel.state.diff(self.merit_runningDatas[-1].xnext, self.xs_try[-1])
+        self.problem.terminalModel.calc(self.merit_terminalData, self.xs_try[-1])
+        self.problem.terminalModel.calcDiff(self.merit_terminalData, self.xs_try[-1])  
+        
+        self.merit_try = self.meritFunction()
+        return self.merit_try
 
-    def meritFunction(self, t, data_previous):
-        raise NotImplementedError("solve method not implemented yet")
+    def meritFunction(self):
+        """ the computation of the cost gradients will lag by one time step, 
+        i.e. at t = 1 we compute dJ/dx_0  
+        we will use t = -1 for terminal state 
+        """
+        if t == -1: 
+            pass 
+
+        return 0.  
 
 
     def solve(self, init_xs=None, init_us=None, init_ys=None, maxiter=1, isFeasible=False, regInit=None):
@@ -161,6 +202,8 @@ class PartialDGSolver(SolverAbstract):
                 except:
                     raise BaseException("Backward Pass Failed")
                 break 
+
+            self.tryStep(1.)
 
 
 
@@ -219,3 +262,6 @@ class PartialDGSolver(SolverAbstract):
         self.P = [np.zeros([m.state.ndx, m.state.ndx]) for m in self.measurement_models()] 
         self.x_grad = [np.zeros(m.state.ndx) for m in self.process_models()]
         self.u_grad = [np.zeros(m.nu) for m in self.problem.runningModels]
+        # 
+        self.merit_runningDatas = [m.createData() for m in self.problem.runningModels]
+        self.merit_terminalData = self.problem.terminalModel.createData()  
