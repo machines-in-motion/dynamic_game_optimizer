fixes few bugs in solver and model derivatives

python/demos/linear_quadratic_partial.py
@@ -25,7 +25,7 @@ mm = 1e-2 * np.eye(4) # measurement error weight matrix
 P0  = 1e-2 * np.eye(4)
 MU = 0.01
 
-t_solve = 1 # solve problem for t = 50 
+t_solve = 20 # solve problem for t = 50 
 
 if __name__ == "__main__":
     lq_diff_running =  lin_quad.DifferentialActionModelLQ()

python/solvers/partial.py
@@ -94,7 +94,7 @@ class PartialDGSolver(SolverAbstract):
             invPt = np.linalg.inv(self.P[t])
             Hxx = self.inv_mu*mdata.Hxx.T.dot(mdata.invR).dot(self.gammas[t]) if t > 0 else 0
             Lxx = pdata.Lxx + self.inv_mu*pmodel.differential.Fxx.T.dot(self.invQ[t+1]).dot(self.ws[t+1]) + Hxx
-            self.E[t+1] = invPt + pdata.Fx.dot(self.invQ[t+1]).dot(pdata.Fx.T) - self.mu*Lxx
+            self.E[t+1] = invPt + pdata.Fx.T.dot(self.invQ[t+1]).dot(pdata.Fx) - self.mu*Lxx
             aux0 = invPt - self.mu*Lxx 
             Lb0 = scl.cho_factor(aux0, lower=True) 
             Pbar =  self.Q[t+1] + pdata.Fx.dot(scl.cho_solve(Lb0, pdata.Fx.T)) 