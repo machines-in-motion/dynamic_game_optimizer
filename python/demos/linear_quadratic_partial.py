fixes numerical stability in coupling and adds plotting scripts

python/demos/linear_quadratic_partial.py
@@ -25,7 +25,7 @@ mm = 1e-2 * np.eye(4) # measurement error weight matrix
 P0  = 1e-2 * np.eye(4)
 MU = 0.01
 
-t_solve = 20 # solve problem for t = 50 
+t_solve = 50 # solve problem for t = 50 
 
 if __name__ == "__main__":
     lq_diff_running =  lin_quad.DifferentialActionModelLQ()
@@ -57,13 +57,13 @@ if __name__ == "__main__":
 
 
     
-    ys = measurement_trajectory.calc(ddp_solver.xs[:t_solve], ddp_solver.us[:t_solve])
+    ys = measurement_trajectory.calc(ddp_solver.xs[:t_solve+1], ddp_solver.us[:t_solve])
     
     dg_solver = PartialDGSolver(ddp_problem, MU, pm, P0, measurement_trajectory)
     print(" Constructor and Data Allocation for Partial Solver Works ".center(LINE_WIDTH, '-'))
 
     u_init = [np.zeros(2)]*horizon
-    u_init[:t_solve-1] = ddp_solver.us[:t_solve-1]
+    u_init[:t_solve] = ddp_solver.us[:t_solve]
     dg_solver.solve(init_xs=xs, init_us=u_init, init_ys=ys)
 
     print(" Plotting DDP and DG Solutions ".center(LINE_WIDTH, '-'))

python/demos/point_cliff_partial.py
@@ -23,9 +23,9 @@ PLOT_DDP = True
 pm = 1e-2 * np.eye(4) # process error weight matrix 
 mm = 1e-2 * np.eye(4) # measurement error weight matrix 
 P0  = 1e-2 * np.eye(4)
-MU = 1.
+MU = .1
 
-t_solve = 100 # solve problem for t = 50 
+t_solve = 50 # solve problem for t = 50 
 
 if __name__ == "__main__":
     cliff_diff_running =  point_cliff.DifferentialActionModelCliff()
@@ -84,13 +84,13 @@ if __name__ == "__main__":
 
     x = np.array(dg_solver.xs)
 
-    for t in range(len(np.array(dg_solver.xs[:t_solve+1]))):
+    for t in range(len(np.array(dg_solver.xs[:t_solve]))):
         if t == 0:
             plt.plot(np.array([x[t][0], x_n[t][0]]), np.array([x[t][1], x_n[t][1]]), 'green', label='DG estimation')
         else:
             plt.plot(np.array([x[t][0], x_n[t][0]]), np.array([x[t][1], x_n[t][1]]), 'green')
 
-    for t_ in range(len(np.array(dg_solver.xs[t_solve+1:]))):
+    for t_ in range(len(np.array(dg_solver.xs[t_solve:]))):
         t = t_ + t_solve -1 
         if t_ == 0:
             plt.plot(np.array([x[t][0], x_n[t][0]]), np.array([x[t][1], x_n[t][1]]), 'red', label='DG control')
@@ -100,7 +100,7 @@ if __name__ == "__main__":
 
     plt.plot(np.array(ddp_solver.xs)[:,0],np.array(ddp_solver.xs)[:,1], label="DDP Trajectory")
 
-    # plt.plot(np.array(ddp_solver.xs)[:t_solve,0],np.array(ddp_solver.xs)[:t_solve,1], 'black', label="Measurements")
+    plt.plot(np.array(ddp_solver.xs)[:t_solve,0],np.array(ddp_solver.xs)[:t_solve,1], 'black', label="Measurements")
     plt.legend()
     plt.show()
     

python/solvers/partial.py
@@ -138,13 +138,15 @@ class PartialDGSolver(SolverAbstract):
 
     def _coupling(self):
         t = self.split_t 
-        if t == self.problem.T:
-            aux = np.eye(self.problem.terminalModel.state.ndx) - self.mu*self.P[t].dot(self.Vxx[t]) 
-        else:
-            aux = np.eye(self.problem.runningModels[t].state.ndx) - self.mu*self.P[t].dot(self.Vxx[t]) 
+        Pinv = np.linalg.inv(self.P[t])
+        aux = Pinv - self.mu*self.Vxx[t] 
+        # if t == self.problem.T:
+        #     aux = Pinv - self.mu*self.Vxx[t] #self.P[t].dot() 
+        # else:
+            # aux = np.eye(self.problem.runningModels[t].state.ndx) - self.mu*self.P[t].dot(self.Vxx[t]) 
         
         Lb = scl.cho_factor(aux, lower=True) 
-        self.dx[t] = scl.cho_solve(Lb, self.mu_hat[t] + self.mu*self.P[t].dot(self.vx[t]))
+        self.dx[t] = scl.cho_solve(Lb, Pinv.dot(self.mu_hat[t]) + self.mu*self.vx[t])
         
 
     def _estimation_backward(self):