86156308f2f6f8354b8771e0c79c7d30c7abee6d
ajordana <aj2988@nyu.edu>
2022-03-14 10:47:57 -0400

    plots folder

--- python/models/pendulum_action.py
+++ python/models/pendulum_action.py
@@ -1,85 +1,84 @@
-import numpy as np 
-import crocoddyl 
-import matplotlib.pyplot as plt 
-LINE_WIDTH = 100 
+import numpy as np
+import crocoddyl
+import matplotlib.pyplot as plt
+
+LINE_WIDTH = 100
 
 
 class PendulumDynamics:
     def __init__(self):
         self.g = 9.81
-        self.mass = 1 
+        self.mass = 1
         self.l = 1
-        self.nx = 2 
+        self.nx = 2
         self.ndx = 2
         self.nu = 1
 
     def nonlinear_dynamics(self, x, u):
-        return - self.g * np.sin(x[0]) / self.l + u / (self.l**2 * self.mass) 
-    
-    
+        return -self.g * np.sin(x[0]) / self.l + u / (self.l ** 2 * self.mass)
+
+
 class DifferentialActionModelPendulum(crocoddyl.DifferentialActionModelAbstract):
-    def __init__(self, dt=1.e-2, isTerminal=False):
+    def __init__(self, dt=1.0e-2, isTerminal=False):
         self.dynamics = PendulumDynamics()
-        state =  crocoddyl.StateVector(self.dynamics.nx)
-        crocoddyl.DifferentialActionModelAbstract.__init__(self, state, self.dynamics.nu, self.dynamics.ndx)
-        self.ndx = self.state.ndx 
+        state = crocoddyl.StateVector(self.dynamics.nx)
+        crocoddyl.DifferentialActionModelAbstract.__init__(
+            self, state, self.dynamics.nu, self.dynamics.ndx
+        )
+        self.ndx = self.state.ndx
         self.isTerminal = isTerminal
-        self.mass = self.dynamics.mass 
-        self.l =  self.dynamics.l 
-        self.g =  self.dynamics.g 
-        self.dt = dt 
+        self.mass = self.dynamics.mass
+        self.l = self.dynamics.l
+        self.g = self.dynamics.g
+        self.dt = dt
         self.Fxx = np.zeros([self.ndx, self.ndx, self.ndx])
         self.Fxu = np.zeros([self.ndx, self.ndx, self.nu])
         self.Fuu = np.zeros([self.ndx, self.nu, self.nu])
 
-
     def _running_cost(self, x, u):
-        cost = 0.5 * u[0]**2 / 100.  
+        cost = 0.5 * u[0] ** 2 / 100.0
         return cost
 
     def _terminal_cost(self, x, u):
-        cost = 2000*((x[0]-np.pi)**2) + 100*(x[1]**2)  
-        return cost 
-     
+        cost = 2000 * ((x[0] - np.pi) ** 2) + 100 * (x[1] ** 2)
+        return cost
+
     def calc(self, data, x, u=None):
-        if u is None: 
+        if u is None:
             u = np.zeros(self.nu)
 
-        if self.isTerminal: 
-            data.cost = self._terminal_cost(x,u) 
+        if self.isTerminal:
+            data.cost = self._terminal_cost(x, u)
             data.xout = np.zeros(1)
         else:
-            data.cost = self._running_cost(x,u)
-            data.xout = self.dynamics.nonlinear_dynamics(x,u)
-
+            data.cost = self._running_cost(x, u)
+            data.xout = self.dynamics.nonlinear_dynamics(x, u)
 
     def calcDiff(self, data, x, u=None):
-        Fx = np.zeros([1,2]) 
-        Fu = np.zeros([1,1])
-        
+        Fx = np.zeros([1, 2])
+        Fu = np.zeros([1, 1])
+
         Lx = np.zeros([2])
         Lu = np.zeros([1])
-        Lxx = np.zeros([2,2])
-        Luu = np.zeros([1,1])
-        Lxu = np.zeros([2,1])
+        Lxx = np.zeros([2, 2])
+        Luu = np.zeros([1, 1])
+        Lxu = np.zeros([2, 1])
         if self.isTerminal:
-            Lx[0] = 4000.*(x[0]-np.pi)
-            Lx[1] = 200.*x[1]   
-            Lxx[0,0] = 4000. 
-            Lxx[1,1] = 200. 
+            Lx[0] = 4000.0 * (x[0] - np.pi)
+            Lx[1] = 200.0 * x[1]
+            Lxx[0, 0] = 4000.0
+            Lxx[1, 1] = 200.0
         else:
-            Lu[0] = u[0] / 100.
-            Luu[0,0] = 1. / 100.
-            # 
-            Fx[0, 0] = - self.g * np.cos(x[0]) / self.l
-            Fu[0,0] = 1./(self.l**2 * self.mass) 
+            Lu[0] = u[0] / 100.0
+            Luu[0, 0] = 1.0 / 100.0
+            Fx[0, 0] = -self.g * np.cos(x[0]) / self.l
+            Fu[0, 0] = 1.0 / (self.l ** 2 * self.mass)
 
-            # I will store the 2nd order derivative of the dynamics here since crocoddyl support it 
-            # this second order derivative will be of x_{t+1} = x_t + dx_t and not the continuous dynamics 
-            
-            self.Fxx[0,0,0] =  self.dt**2 * self.g * np.sin(x[0]) / self.l
-            self.Fxx[1,0,0] =  self.dt  * self.g * np.sin(x[0]) / self.l
+            # I will store the 2nd order derivative of the dynamics here since crocoddyl support it
+            # this second order derivative will be of x_{t+1} = x_t + dx_t and not the continuous dynamics
 
+            self.Fxx[0, 0, 0] = self.dt ** 2 * self.g * np.sin(x[0]) / self.l
+            self.Fxx[1, 0, 0] = self.dt * self.g * np.sin(x[0]) / self.l
 
         data.Fx = Fx.copy()
         data.Fu = Fu.copy()
@@ -90,50 +89,34 @@ class DifferentialActionModelPendulum(crocoddyl.DifferentialActionModelAbstract)
         data.Lxu = Lxu.copy()
 
 
-            
-     
-if __name__ =="__main__":
-    print(" Testing Point Mass Cliff with DDP ".center(LINE_WIDTH, '#'))
-    Pendulum_diff_running =  DifferentialActionModelPendulum()
+if __name__ == "__main__":
+    print(" Testing Point Mass Cliff with DDP ".center(LINE_WIDTH, "#"))
+    Pendulum_diff_running = DifferentialActionModelPendulum()
     Pendulum_diff_terminal = DifferentialActionModelPendulum(isTerminal=True)
-    print(" Constructing differential models completed ".center(LINE_WIDTH, '-'))
-    dt = 0.01 
-    T = 100 
-    x0 = np.zeros(2) 
+    print(" Constructing differential models completed ".center(LINE_WIDTH, "-"))
+    dt = 0.01
+    T = 100
+    x0 = np.zeros(2)
     MAX_ITER = 1000
-    Pendulum_running = crocoddyl.IntegratedActionModelEuler(Pendulum_diff_running, dt) 
-    Pendulum_terminal = crocoddyl.IntegratedActionModelEuler(Pendulum_diff_terminal, dt) 
-    print(" Constructing integrated models completed ".center(LINE_WIDTH, '-'))
+    Pendulum_running = crocoddyl.IntegratedActionModelEuler(Pendulum_diff_running, dt)
+    Pendulum_terminal = crocoddyl.IntegratedActionModelEuler(Pendulum_diff_terminal, dt)
+    print(" Constructing integrated models completed ".center(LINE_WIDTH, "-"))
 
-    problem = crocoddyl.ShootingProblem(x0, [Pendulum_running]*T, Pendulum_terminal)
-    print(" Constructing shooting problem completed ".center(LINE_WIDTH, '-'))
-    
+    problem = crocoddyl.ShootingProblem(x0, [Pendulum_running] * T, Pendulum_terminal)
+    print(" Constructing shooting problem completed ".center(LINE_WIDTH, "-"))
+
     ddp = crocoddyl.SolverDDP(problem)
-    print(" Constructing DDP solver completed ".center(LINE_WIDTH, '-'))
-    ddp.setCallbacks([
-    crocoddyl.CallbackLogger(),
-    crocoddyl.CallbackVerbose()
-    ])
-    xs = [x0]*(T+1)
-    us = [np.zeros(1)]*T
-    converged = ddp.solve(xs,us, MAX_ITER)
-    theta =[]  
-    theta_dot =[]
-    time_array = dt*np.arange(T+1)
-    for xi in ddp.xs:
-        theta += [xi[0]]
-        theta_dot += [xi[1]]
+    print(" Constructing DDP solver completed ".center(LINE_WIDTH, "-"))
+    ddp.setCallbacks([crocoddyl.CallbackLogger(), crocoddyl.CallbackVerbose()])
+    xs = [x0] * (T + 1)
+    us = [np.zeros(1)] * T
+    converged = ddp.solve(xs, us, MAX_ITER)
+    time_array = dt * np.arange(T + 1)
     if converged:
-        print(" DDP solver has CONVERGED ".center(LINE_WIDTH, '-'))
-        plt.figure("trajectory plot")
-        # plt.plot(x,y)
-        # plt.plot(np.array(ddp.xs)[:,0],np.array(ddp.xs)[:,1], label="ddp")
-        plt.plot(np.array(ddp.xs)[:,0], label="ddp")
-
-        # plt.figure("velocity plots")
-        # plt.plot(time_array, vx)
-        # plt.plot(time_array, vy)
+        print(" DDP solver has CONVERGED ".center(LINE_WIDTH, "-"))
+        plt.figure()
+        plt.plot(np.array(ddp.xs)[:, 0], label="ddp")
+        plt.xlabel("Time")
+        plt.ylabel("$\\theta$ ")
+        plt.title("DDP")
         plt.show()
-
-
-    
--- python/models/point_cliff_action.py
+++ python/models/point_cliff_action.py
@@ -1,119 +1,121 @@
-""" this example is inspired by risk sensitive paper from Farbod """
+""" This example is inspired by the risk sensitive paper from Farbod """
 
-import numpy as np 
-import crocoddyl 
-import matplotlib.pyplot as plt 
-LINE_WIDTH = 100 
+import numpy as np
+import crocoddyl
+import matplotlib.pyplot as plt
+
+LINE_WIDTH = 100
 
 
 class PointMassDynamics:
     def __init__(self):
-        self.g = np.array([0. , -9.81])
-        self.mass = 1 
-        self.nq = 2 
-        self.nv = 2 
-        self.ndx = 2 
-        self.nx = self.nq + self.nv 
-        self.nu = 2 
-        self.c_drag = .02
+        self.g = np.array([0.0, -9.81])
+        self.mass = 1
+        self.nq = 2
+        self.nv = 2
+        self.ndx = 2
+        self.nx = self.nq + self.nv
+        self.nu = 2
+        self.c_drag = 0.001
 
     def nonlinear_dynamics(self, x, u):
-        return (1/self.mass)*u + self.g - self.c_drag*x[2:]**2
-    
+        return (1 / self.mass) * u + self.g - self.c_drag * x[2:] ** 2
+
     def derivatives(self, x, u):
-        """returns df/dx evaluated at x,u """
-        dfdx = np.zeros([self.nv ,self.ndx])
-        dfdu = np.zeros([self.nv ,self.nu])
-        dfdu[0,0] = 1./self.mass 
-        dfdu[1,1] = 1./self.mass 
+        """returns df/dx evaluated at x,u"""
+        dfdx = np.zeros([self.nv, self.ndx])
+        dfdu = np.zeros([self.nv, self.nu])
+        dfdu[0, 0] = 1.0 / self.mass
+        dfdu[1, 1] = 1.0 / self.mass
         return dfdx, dfdu
-    
+
+
 class DifferentialActionModelCliff(crocoddyl.DifferentialActionModelAbstract):
-    def __init__(self, dt=1.e-2, isTerminal=False):
+    def __init__(self, dt=1.0e-2, isTerminal=False):
         self.dynamics = PointMassDynamics()
-        state =  crocoddyl.StateVector(self.dynamics.nx)
-        crocoddyl.DifferentialActionModelAbstract.__init__(self, state, self.dynamics.nu, self.dynamics.ndx)
-        self.ndx = self.state.ndx 
+        state = crocoddyl.StateVector(self.dynamics.nx)
+        crocoddyl.DifferentialActionModelAbstract.__init__(
+            self, state, self.dynamics.nu, self.dynamics.ndx
+        )
+        self.ndx = self.state.ndx
         self.isTerminal = isTerminal
-        self.mass = 1. 
-        self.cost_scale = 1.e-1
-        self.dt = dt 
+        self.mass = 1.0
+        self.cost_scale = 1.0e-1
+        self.dt = dt
         self.Fxx = np.zeros([self.ndx, self.ndx, self.ndx])
         self.Fxu = np.zeros([self.ndx, self.ndx, self.nu])
         self.Fuu = np.zeros([self.ndx, self.nu, self.nu])
 
-
     def _running_cost(self, x, u):
-        cost = 0.1/((.1*x[1] + 1.)**10) + u[0]**2 + .01*u[1]**2 
-        # cost = 10*((x[0]-10.)**2) + 10*(x[1]**2) + 1*(x[2]**2) + 1*(x[3]**2)   + u[0]**2 + .01*u[1]**2 
+        cost = 0.1 / ((0.1 * x[1] + 1.0) ** 10) + u[0] ** 2 + 0.01 * u[1] ** 2
         cost *= self.cost_scale
         return cost
 
     def _terminal_cost(self, x, u):
-        cost = 20000*((x[0]-10.)**2) + 20000*(x[1]**2) + 1000*(x[2]**2) + 1000*(x[3]**2)  
+        cost = (
+            20000 * ((x[0] - 10.0) ** 2)
+            + 20000 * (x[1] ** 2)
+            + 1000 * (x[2] ** 2)
+            + 1000 * (x[3] ** 2)
+        )
         cost *= self.cost_scale
-        return cost 
-     
+        return cost
+
     def calc(self, data, x, u=None):
-        if u is None: 
+        if u is None:
             u = np.zeros(self.nu)
 
-        
-        if self.isTerminal: 
-            data.cost = self._terminal_cost(x,u) 
+        if self.isTerminal:
+            data.cost = self._terminal_cost(x, u)
             data.xout = np.zeros(2)
         else:
-            data.cost = self._running_cost(x,u)
-            data.xout = self.dynamics.nonlinear_dynamics(x,u)
+            data.cost = self._running_cost(x, u)
+            data.xout = self.dynamics.nonlinear_dynamics(x, u)
 
-        # data.r = None # residuals I think, Must be crucial for finite difference derivative computation, 
-        # must check it  
-    
     def calcDiff(self, data, x, u=None):
-        Fx = np.zeros([2,4]) 
-        Fu = np.zeros([2,2])
-        
+        Fx = np.zeros([2, 4])
+        Fu = np.zeros([2, 2])
+
         Lx = np.zeros([4])
         Lu = np.zeros([2])
-        Lxx = np.zeros([4,4])
-        Luu = np.zeros([2,2])
-        Lxu = np.zeros([4,2])
+        Lxx = np.zeros([4, 4])
+        Luu = np.zeros([2, 2])
+        Lxu = np.zeros([4, 2])
         if self.isTerminal:
-            Lx[0] = 40000.*(x[0]-10)
-            Lx[1] = 40000.*x[1]
-            Lx[2] = 2000.*x[2]
-            Lx[3] = 2000.*x[3]     
-            Lxx[0,0] = 40000. 
-            Lxx[1,1] = 40000. 
-            Lxx[2,2] = 2000. 
-            Lxx[3,3] = 2000. 
-            Lx *= self.cost_scale 
-            Lxx *= self.cost_scale 
+            Lx[0] = 40000.0 * (x[0] - 10)
+            Lx[1] = 40000.0 * x[1]
+            Lx[2] = 2000.0 * x[2]
+            Lx[3] = 2000.0 * x[3]
+            Lxx[0, 0] = 40000.0
+            Lxx[1, 1] = 40000.0
+            Lxx[2, 2] = 2000.0
+            Lxx[3, 3] = 2000.0
+            Lx *= self.cost_scale
+            Lxx *= self.cost_scale
         else:
-            Lx[1] = - 0.1 /((1 + .1*x[1])**11)
-            Lu[0] = 2.*u[0] 
+            Lx[1] = -0.1 / (1 + 0.1 * x[1]) ** 11
+            Lu[0] = 2.0 * u[0]
             Lu[1] = 0.02 * u[1]
-            Lxx[1,1] = 0.11/((.1*x[1]+1.)**12)
-            Luu[0,0] = 2. 
-            Luu[1,1] = 0.02
-            # 
-            Lx *= self.cost_scale 
-            Lxx *= self.cost_scale 
-            Lu *= self.cost_scale 
-            Luu *= self.cost_scale 
-            # 
-            Fu[0,0] = 1./self.mass 
-            Fu[1,1] = 1./self.mass 
-            # 
-            Fx[0,2] = -2.*self.dynamics.c_drag*x[2]
-            Fx[1,3] =  -2.*self.dynamics.c_drag*x[3]
-            # I will store the 2nd order derivative of the dynamics here since crocoddyl support it 
-            # this second order derivative will be of x_{t+1} = x_t + dx_t and not the continuous dynamics 
-            self.Fxx[0,2,2] = -2 * self.dynamics.c_drag * self.dt**2  
-            self.Fxx[1,3,3] = -2 * self.dynamics.c_drag * self.dt**2  
-            self.Fxx[2,2,2] = -2 * self.dynamics.c_drag * self.dt 
-            self.Fxx[3,3,3] = -2 * self.dynamics.c_drag * self.dt 
-
+            Lxx[1, 1] = 0.11 / (0.1 * x[1] + 1.0) ** 12
+            Luu[0, 0] = 2.0
+            Luu[1, 1] = 0.02
+            #
+            Lx *= self.cost_scale
+            Lxx *= self.cost_scale
+            Lu *= self.cost_scale
+            Luu *= self.cost_scale
+            #
+            Fu[0, 0] = 1.0 / self.mass
+            Fu[1, 1] = 1.0 / self.mass
+            #
+            Fx[0, 2] = -2.0 * self.dynamics.c_drag * x[2]
+            Fx[1, 3] = -2.0 * self.dynamics.c_drag * x[3]
+            # I will store the 2nd order derivative of the dynamics here since crocoddyl support it
+            # this second order derivative will be of x_{t+1} = x_t + dx_t and not the continuous dynamics
+            self.Fxx[0, 2, 2] = -2 * self.dynamics.c_drag * self.dt ** 2
+            self.Fxx[1, 3, 3] = -2 * self.dynamics.c_drag * self.dt ** 2
+            self.Fxx[2, 2, 2] = -2 * self.dynamics.c_drag * self.dt
+            self.Fxx[3, 3, 3] = -2 * self.dynamics.c_drag * self.dt
 
         data.Fx = Fx.copy()
         data.Fu = Fu.copy()
@@ -124,53 +126,32 @@ class DifferentialActionModelCliff(crocoddyl.DifferentialActionModelAbstract):
         data.Lxu = Lxu.copy()
 
 
-            
-     
-if __name__ =="__main__":
-    print(" Testing Point Mass Cliff with DDP ".center(LINE_WIDTH, '#'))
-    cliff_diff_running =  DifferentialActionModelCliff()
+if __name__ == "__main__":
+    print(" Testing Point Mass Cliff with DDP ".center(LINE_WIDTH, "#"))
+    cliff_diff_running = DifferentialActionModelCliff()
     cliff_diff_terminal = DifferentialActionModelCliff(isTerminal=True)
-    print(" Constructing differential models completed ".center(LINE_WIDTH, '-'))
-    dt = 0.01 
-    T = 300 
-    x0 = np.zeros(4) 
-    MAX_ITER = 1000
-    cliff_running = crocoddyl.IntegratedActionModelEuler(cliff_diff_running, dt) 
-    cliff_terminal = crocoddyl.IntegratedActionModelEuler(cliff_diff_terminal, dt) 
-    print(" Constructing integrated models completed ".center(LINE_WIDTH, '-'))
+    print(" Constructing differential models completed ".center(LINE_WIDTH, "-"))
+    dt = 0.01
+    T = 300
+    x0 = np.zeros(4)
+    cliff_running = crocoddyl.IntegratedActionModelEuler(cliff_diff_running, dt)
+    cliff_terminal = crocoddyl.IntegratedActionModelEuler(cliff_diff_terminal, dt)
+    print(" Constructing integrated models completed ".center(LINE_WIDTH, "-"))
 
-    problem = crocoddyl.ShootingProblem(x0, [cliff_running]*T, cliff_terminal)
-    print(" Constructing shooting problem completed ".center(LINE_WIDTH, '-'))
-    
+    problem = crocoddyl.ShootingProblem(x0, [cliff_running] * T, cliff_terminal)
+    print(" Constructing shooting problem completed ".center(LINE_WIDTH, "-"))
+
     ddp = crocoddyl.SolverDDP(problem)
-    print(" Constructing DDP solver completed ".center(LINE_WIDTH, '-'))
-    ddp.setCallbacks([
-    crocoddyl.CallbackLogger(),
-    crocoddyl.CallbackVerbose()
-    ])
-    xs = [x0]*(T+1)
-    us = [np.zeros(2)]*T
-    converged = ddp.solve(xs,us, MAX_ITER)
-    x =[]
-    y =[]
-    vx =[]
-    vy =[]
-    time_array = dt*np.arange(T+1)
-    for xi in ddp.xs:
-        x += [xi[0]]
-        y += [xi[1]]
-        vx += [xi[2]]
-        vy += [xi[3]]
+    print(" Constructing DDP solver completed ".center(LINE_WIDTH, "-"))
+    ddp.setCallbacks([crocoddyl.CallbackLogger(), crocoddyl.CallbackVerbose()])
+    xs = [x0] * (T + 1)
+    us = [np.zeros(2)] * T
+    converged = ddp.solve(xs, us)
     if converged:
-        print(" DDP solver has CONVERGED ".center(LINE_WIDTH, '-'))
+        print(" DDP solver has CONVERGED ".center(LINE_WIDTH, "-"))
         plt.figure("trajectory plot")
-        # plt.plot(x,y)
-        plt.plot(np.array(ddp.xs)[:,0],np.array(ddp.xs)[:,1], label="ddp")
-
-        # plt.figure("velocity plots")
-        # plt.plot(time_array, vx)
-        # plt.plot(time_array, vy)
+        plt.plot(np.array(ddp.xs)[:, 0], np.array(ddp.xs)[:, 1], label="ddp")
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.title("DDP")
         plt.show()
-
-
-    
