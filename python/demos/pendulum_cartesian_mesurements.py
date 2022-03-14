f4aacd3d013dbac8cc47c8617a66d69cada61ea9
ajordana <aj2988@nyu.edu>
2022-03-14 13:23:51 -0400

    new parameters

--- python/demos/pendulum_cartesian_mesurements.py
+++ python/demos/pendulum_cartesian_mesurements.py
@@ -20,7 +20,7 @@ PLOT_DDP = True
 pm = 1e-2 * np.eye(2) # process error weight matrix 
 mm = 1e0 * np.eye(2) # measurement error weight matrix 
 P0  = 1e-2 * np.eye(2)
-MU = 0.1
+MU = 0.02
 
 t_solve = 15
 
