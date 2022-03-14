fixed line search and added second order derivatives

python/demos/pendulum.py
@@ -11,14 +11,14 @@ import matplotlib.pyplot as plt
 from solvers import full
 
 
-horizon = 100
+horizon = 30
 plan_dt = 1e-2
 x0 = np.zeros(2)
 Q = 1e-2 * np.eye(2)
-mu = 0.2
+mu = 0.01
 
 
-MAX_ITER = 1000
+MAX_ITER = 10000
 LINE_WIDTH = 100
 
 pendulum_diff_running = pendulum.DifferentialActionModelPendulum()
@@ -51,7 +51,6 @@ for t in range(len(np.array(dg_solver.xs[:-1]))):
 plt.xlabel("Time")
 plt.ylabel("$\\theta$")
 plt.title("$\mu = $ " + str(mu))
-plt.show()
 
 plt.figure("Phase portrait")
 for t in range(len(np.array(dg_solver.xs[:-1]))):

python/models/pendulum_action.py
@@ -19,7 +19,7 @@ class PendulumDynamics:
 
 
 class DifferentialActionModelPendulum(crocoddyl.DifferentialActionModelAbstract):
-    def __init__(self, dt=1.0e-2, isTerminal=False):
+    def __init__(self, dt=1e-2, isTerminal=False):
         self.dynamics = PendulumDynamics()
         state = crocoddyl.StateVector(self.dynamics.nx)
         crocoddyl.DifferentialActionModelAbstract.__init__(
@@ -40,7 +40,7 @@ class DifferentialActionModelPendulum(crocoddyl.DifferentialActionModelAbstract)
         return cost
 
     def _terminal_cost(self, x, u):
-        cost = 2000 * ((x[0] - np.pi) ** 2) + 100 * (x[1] ** 2)
+        cost = 2000 * (x[0] - np.pi) ** 2 + 100 * x[1] ** 2
         return cost
 
     def calc(self, data, x, u=None):
@@ -64,13 +64,13 @@ class DifferentialActionModelPendulum(crocoddyl.DifferentialActionModelAbstract)
         Luu = np.zeros([1, 1])
         Lxu = np.zeros([2, 1])
         if self.isTerminal:
-            Lx[0] = 4000.0 * (x[0] - np.pi)
-            Lx[1] = 200.0 * x[1]
-            Lxx[0, 0] = 4000.0
-            Lxx[1, 1] = 200.0
+            Lx[0] = 40000.0 * (x[0] - np.pi)
+            Lx[1] = 2000.0 * x[1]
+            Lxx[0, 0] = 40000.0
+            Lxx[1, 1] = 2000.0
         else:
-            Lu[0] = u[0] / 100.0
-            Luu[0, 0] = 1.0 / 100.0
+            Lu[0] = u[0] / 10.0
+            Luu[0, 0] = 1.0 / 10.0
             Fx[0, 0] = -self.g * np.cos(x[0]) / self.l
             Fu[0, 0] = 1.0 / (self.l ** 2 * self.mass)
 

python/solvers/full.py
@@ -89,9 +89,9 @@ class SaddlePointSolver(SolverAbstract):
         self.Vxx[-1][:,:] = self.problem.terminalData.Lxx
         self.vx[-1][:] = self.problem.terminalData.Lx 
         for t, (model, data) in rev_enumerate(zip(self.problem.runningModels,self.problem.runningDatas)):
-            Lxx = data.Lxx #+ self.inv_mu*model.differential.Fxx.T.dot(self.invQ[t+1]).dot(self.ws[t+1])
-            Lux = data.Lxu.T # + self.inv_mu*model.differential.Fxu.T.dot(self.invQ[t+1]).dot(self.ws[t+1])
-            Luu = data.Luu #+ self.inv_mu*model.differential.Fuu.T.dot(self.invQ[t+1]).dot(self.ws[t+1])
+            Lxx = data.Lxx + self.inv_mu*model.differential.Fxx.T.dot(self.invQ[t+1]).dot(self.ws[t+1])
+            Lux = data.Lxu.T + self.inv_mu*model.differential.Fxu.T.dot(self.invQ[t+1]).dot(self.ws[t+1])
+            Luu = data.Luu + self.inv_mu*model.differential.Fuu.T.dot(self.invQ[t+1]).dot(self.ws[t+1])
             aux0 = np.eye(model.state.ndx) - self.mu*self.Vxx[t+1].dot(self.Q[t+1]) 
             Lb = scl.cho_factor(aux0, lower=True) 
             aux1 = scl.cho_solve(Lb, self.Vxx[t+1])
@@ -185,7 +185,7 @@ class SaddlePointSolver(SolverAbstract):
                     continue 
 
 
-                if dV > self.merit / 4:
+                if dV > a * self.merit / 4:
                     print("step accepted for alpha = %s \n new merit is %s"%(a, self.merit_try))
                     self.setCandidate(self.xs_try, self.us_try, self.isFeasible) 
                     self.merit = self.merit_try